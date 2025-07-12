d402eace561b73b4c19b1880bdb9c16e153b5a88
sangook <ksangook@gmail.com>
2025-07-13 01:38:01 +0900

    DAP(codelldb), change keymap
    Signed-off-by: sangook <ksangook@gmail.com>

--- lua/custom/c_keymaps.lua
+++ lua/custom/c_keymaps.lua
@@ -1,17 +1,27 @@
--- Toggle virtual_text or virtual_lines
-vim.keymap.set('n', '<M-d>', function()
-  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end, { desc = 'Toggle Diagnostic' })
+local map = vim.keymap.set
 
--- Smart Split (Tmux Moving)
-vim.keymap.set({ 't', 'n' }, '<C-h>', '<cmd>SmartCursorMoveLeft<cr>')
-vim.keymap.set({ 't', 'n' }, '<C-j>', '<cmd>SmartCursorMoveDown<cr>')
-vim.keymap.set({ 't', 'n' }, '<C-k>', '<cmd>SmartCursorMoveUp<cr>')
-vim.keymap.set({ 't', 'n' }, '<C-l>', '<cmd>SmartCursorMoveRight<cr>')
-vim.keymap.set({ 't', 'n' }, '<M-h>', '<cmd>SmartResizeLeft<cr>')
-vim.keymap.set({ 't', 'n' }, '<M-j>', '<cmd>SmartResizeDown<cr>')
-vim.keymap.set({ 't', 'n' }, '<M-k>', '<cmd>SmartResizeUp<cr>')
-vim.keymap.set({ 't', 'n' }, '<M-l>', '<cmd>SmartResizeRight<cr>')
+-- Diagnostic virtualtext or virtuallines
+map('n', '<leader>tl', function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = 'LSP: [T]oggle Error [L]ens' })
+
+-- Transparent
+map('n', '<leader>tt', '<cmd>TransparentToggle<cr>', { desc = '[T]oggle [T]ransparent' })
+
+-- Smart Split
+map({ 't', 'n' }, '<C-h>', '<cmd>SmartCursorMoveLeft<cr>')
+map({ 't', 'n' }, '<C-j>', '<cmd>SmartCursorMoveDown<cr>')
+map({ 't', 'n' }, '<C-k>', '<cmd>SmartCursorMoveUp<cr>')
+map({ 't', 'n' }, '<C-l>', '<cmd>SmartCursorMoveRight<cr>')
+map({ 't', 'n' }, '<M-h>', '<cmd>SmartResizeLeft<cr>')
+map({ 't', 'n' }, '<M-j>', '<cmd>SmartResizeDown<cr>')
+map({ 't', 'n' }, '<M-k>', '<cmd>SmartResizeUp<cr>')
+map({ 't', 'n' }, '<M-l>', '<cmd>SmartResizeRight<cr>')
 
 -- Smart Translation
-vim.keymap.set({ 'n', 'v' }, '<M-t>', ':Translate<cr>')
+map({ 'n', 'v' }, '<M-t>', ':Translate<cr>')
+
+-- Buffers
+map('n', '<leader>x', '<cmd>bd<cr>', { desc = '[B]uffer Delete' })
+map('n', '<leader>n', '<cmd>bn<cr>', { desc = '[B]uffer Next' })
+map('n', '<leader>p', '<cmd>bp<cr>', { desc = '[B]uffer Prev' })
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', '<cmd>Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
